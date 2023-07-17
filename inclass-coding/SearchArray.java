39279a15c25330bf700cf55de716b6e4fdcc34ce
leonchoi53 <leonchoi75104@gmail.com>
2023-07-17 15:01:52 +0800

    update 7/17 3

--- inclass-coding/SearchArray.java
+++ inclass-coding/SearchArray.java
@@ -25,5 +25,23 @@ public class SearchArray {
         System.out.println("the maximum value is: " + max);
       }
     }
+
+    int[] nums2 = new int[] {240, 800, -23, 6, 98};
+    int min = 0;
+    for (int i = 0; i < nums2.length; i++){
+      if (nums2[i] < min){
+        min = nums2[i];
+    System.out.println("the maximum value is: " + min);
+      }
+    }
+
+    int[] nums3 = new int[] {1, 2, 3, 4, 5};
+    int min2 = 999999;
+    for (int i = 0; i < nums3.length; i++){
+      if (nums3[i] < min2){
+        min2 = nums3[i];
+    System.out.println("the maximum value is: " + min2);
+      }
+    }
   }
 }
