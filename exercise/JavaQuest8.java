d7b9b36b049d067c3cb7a15f786a9edbbc6ef002
leonchoi53 <leonchoi75104@gmail.com>
2023-07-17 17:39:51 +0800

    update with exercise 7/17

--- exercise/JavaQuest8.java
+++ exercise/JavaQuest8.java
@@ -13,6 +13,45 @@ public class JavaQuest8 {
     // Your program should be able to handle all the above test case.
     int max = 0;
     int max2 = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] >= max) {
+        max2 = max;
+        max = nums[i];
+      }
+      if (max > nums[i] && nums[i] >= max2){
+        max2 = nums[i];
+      }
+      }
+    System.out.println("the second max value is " + max2);
+
+    max = 0;
+    max2 = 0;
+    for (int i = 0; i < nums2.length; i++) {
+      if (nums2[i] >= max) {
+        max2 = max;
+        max = nums2[i];
+      }
+      if (max > nums2[i] && nums2[i] >= max2){
+        max2 = nums2[i];
+      }
+      }
+    System.out.println("the second max value is " + max2);
+
+    max = 0;
+    max2 = 0;
+    for (int i = 0; i < nums3.length; i++) {
+      if (nums3[i] >= max) {
+        max2 = max;
+        max = nums3[i];
+      }
+      if (max > nums3[i] && nums3[i] >= max2){
+        max2 = nums3[i];
+      }
+      }
+    System.out.println("the second max value is " + max2);
+
+    max = 0;
+    max2 = 0;
     for (int i = 0; i < nums4.length; i++) {
       if (nums4[i] >= max) {
         max2 = max;
@@ -23,7 +62,5 @@ public class JavaQuest8 {
       }
       }
     System.out.println("the second max value is " + max2);
-    
-
   }
 }
